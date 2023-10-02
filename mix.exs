3572e2a482875e2c066ade19cb4dcbcba2f9e726
Frank Hunleth <fhunleth@troodon-software.com>
2023-10-02 12:35:43 -0400

    Refresh mix.exs to latest template

--- mix.exs
+++ mix.exs
@@ -1,17 +1,24 @@
 defmodule IntelHex.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/fhunleth/intel_hex"
+
   def project do
     [
       app: :intel_hex,
-      version: "0.1.0",
-      elixir: "~> 1.6",
-      description: "Decode Intel Hex formatted files",
-      source_url: "https://github.com/fhunleth/intel_hex",
-      docs: [extras: ["README.md"], main: "readme"],
+      version: @version,
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      make_targets: ["all"],
+      make_clean: ["mix_clean"],
+      make_error_message: "",
+      description: description(),
+      package: package(),
+      docs: docs(),
+      dialyzer: dialyzer(),
       deps: deps(),
-      package: package()
+      preferred_cli_env: %{docs: :docs, "hex.build": :docs, "hex.publish": :docs}
     ]
   end
 
@@ -21,15 +28,43 @@ defmodule IntelHex.MixProject do
 
   defp deps() do
     [
-      {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
+      {:ex_doc, "~> 0.22", only: :docs, runtime: false},
+      {:dialyxir, "~> 1.1", only: :dev, runtime: false},
+      {:credo, "~> 1.5", only: :dev, runtime: false}
     ]
   end
 
-  defp package() do
+  defp docs do
     [
-      maintainers: ["Frank Hunleth"],
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+    ]
+  end
+
+  defp description do
+    "Decode Intel Hex formatted files"
+  end
+
+  defp package do
+    [
+      files: [
+        "CHANGELOG.md",
+        "lib",
+        "LICENSE",
+        "mix.exs",
+        "README.md"
+      ],
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/fhunleth/intel_hex"}
+      links: %{"Github" => @source_url}
+    ]
+  end
+
+  defp dialyzer() do
+    [
+      flags: [:missing_return, :extra_return, :unmatched_returns, :error_handling, :underspecs]
     ]
   end
 end
